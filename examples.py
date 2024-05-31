40aefb43563fe35b54b80344ea40cd58b9e3f079
Ema Djedovic <ema.djedovic27@gmail.com>
2024-05-31 21:09:44 +0200

    details

--- examples.py
+++ examples.py
@@ -3,7 +3,7 @@ import lippy as lp
 
 # ------------------------------------------------------------
 
-# PRIMJER I
+# PRIMJER
 # funkcija cilja Z = 3x1 + 3x2 + 7x3
 
 c_vektor = [3, 3, 7]
@@ -27,16 +27,16 @@ print("Vrijednost funkcije cilja: ", rjesenje[1])
 
 # ------------------------------------------------------------
 
-# PRIMJER II
+# PRIMJER (PREZENTACIJA)
 # funkcija cilja Z = 7x1 + 9x2
 
 c_vektor = [7, 9]
 
 # ogranicenja
 
-a_matrica = [
-    [-1, 3],
-    [7, 1]]
+a_matrica = [[-1, 3],
+             [7, 1]]
+
 b_vektor = [6, 35]
 
 gomory = lp.CuttingPlaneMethod(c_vektor, a_matrica, b_vektor)
@@ -58,12 +58,12 @@ c_vektor = [5, 6, 8]
 
 # ogranicenja
 
-a_matrica = [
-    [12, 15, 20],
-    [0.5, 0.7, 1],
-    [-1, 0, 0],
-    [0, -1, 0],
-    [0, 0, -1]]
+a_matrica = [[12, 15, 20],
+            [0.5, 0.7, 1],
+            [-1, 0, 0],
+            [0, -1, 0],
+            [0, 0, -1]]
+
 b_vektor = [2000, 100, -10, -5, -8]
 
 gomory = lp.CuttingPlaneMethod(c_vektor, a_matrica, b_vektor)
@@ -74,3 +74,27 @@ rjesenje = gomory.solve()
 print()
 print("Optimalan broj knjiga za svaku kategoriju: ", rjesenje[0])
 print("Maksimalni profit: ", rjesenje[1])
+
+# ------------------------------------------------------------
+
+# PRIMJER 8.10 (Wolsey)
+# funkcija cilja Z = 4x1 - x2
+
+c_vektor = [4, -1]
+
+# ogranicenja
+
+a_matrica = [[7, -2],
+            [0, 1],
+            [2, -2]]
+
+b_vektor = [14, 3, 3]
+
+gomory = lp.CuttingPlaneMethod(c_vektor, a_matrica, b_vektor)
+rjesenje = gomory.solve()
+
+# ispis rezultata
+
+print()
+print("Optimalne vrijednosti varijabli: ", rjesenje[0])
+print("Vrijednost funkcije cilja: ", rjesenje[1])
