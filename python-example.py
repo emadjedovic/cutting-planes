52f79b34c7350b78f8bc29b069065526feeaa9ac
Ema Djedovic <ema.djedovic27@gmail.com>
2024-05-31 11:37:37 +0200

    merged two presentations into one

--- python-example.py
+++ python-example.py
@@ -1,6 +1,9 @@
 import lippy as lp
-# lippy je Python biblioteka za rjesavanje problema linearnog programiranja
+# biblioteka za rjesavanje problema linearnog programiranja
 
+# ------------------------------------------------------------
+
+# PRIMJER
 # funkcija cilja Z = 3x1 + 3x2 + 7x3
 
 c_vektor = [3, 3, 7]
@@ -21,3 +24,29 @@ rjesenje = gomory.solve()
 print()
 print("Optimalne vrijednosti varijabli: ", rjesenje[0])
 print("Vrijednost funkcije cilja: ", rjesenje[1])
+
+# ------------------------------------------------------------
+
+# OPTIMIZACIJA KNJIZARE
+# funkcija cilja Z = 5x1 + 6x2 + 8x3
+
+c_vektor = [5, 6, 8]
+
+# ogranicenja
+
+a_matrica = [
+    [12, 15, 20],
+    [0.5, 0.7, 1],
+    [-1, 0, 0],
+    [0, -1, 0],
+    [0, 0, -1]]
+b_vektor = [2000, 100, -10, -5, -8]
+
+gomory = lp.CuttingPlaneMethod(c_vektor, a_matrica, b_vektor)
+rjesenje = gomory.solve()
+
+# ispis rezultata
+
+print()
+print("Optimalan broj knjiga za svaku kategoriju: ", rjesenje[0])
+print("Maksimalni profit: ", rjesenje[1])
